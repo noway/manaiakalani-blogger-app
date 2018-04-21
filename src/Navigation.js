dd6e8ffcd1c9ddfcf0dad9d8579de332641e632d
Ivan <ivan.miskovic@xero.com>
2018-04-21 21:43:10 +1200

    New nav design start

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,22 +1,28 @@
 import React, { Component } from 'react';
-import NavCss from './NavCss.css';
-import Block from './Block';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Home from './Home';
 import Add from './Add';
 import Profile from './Profile';
+import './nav.css';
 
 class Navigation extends Component {
   render() {
-    const { posts } = this.props;
+    const { posts, onLogoutClick } = this.props;
     return (
       <Router>
         <div>
-          <div className="sidenav">
-            <Link to="/">Home</Link>
-            <Link to="/Add">Add</Link>
-            <Link to="/Profile">Profile</Link>
-          </div>
+          <nav className="nav">
+            <Link to="/" className="nav-icon" title="Home">
+              <i class="fas fa-home"></i>
+            </Link>
+            <Link to="/Add" className="nav-icon" title="Add">
+              <i class="fas fa-plus-circle"></i>
+            </Link>
+            <button className="nav-icon button-logout" onClick={onLogoutClick}>
+              <i class="fas fa-user"></i>
+              <span className="nav-logout-text">Logout</span>
+            </button>
+          </nav>
           <div className="main">
             <Route exact path="/" component={() => (
               <Home posts={posts} />
