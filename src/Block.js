a26b4d41fc7116d82f6366d29f1f88dfade953b9
Akshay Deshmukh <akshaydeshmukh5@yahoo.com>
2018-04-22 12:02:01 +1200

    add page

--- src/Block.js
+++ src/Block.js
@@ -1,22 +1,32 @@
 import React from 'react';
 import Circle from './Circle';
 
+
 const statusToColor = {
     'SCHEDULED': 'yellow',
     'DRAFT': 'red',
     'LIVE': 'green'
 };
+ 
 
-export default function Block({title, status, content}) {
+export default function Block({title, status, content , updated}) {
+    var newdate = Date.parse(updated)
+    var testDate = new Date(newdate);
+    var UpdateDate  =   testDate.getDate() + "/" + (testDate.getMonth() + 1)+ "/" + testDate.getFullYear();
     const color = statusToColor[status];
     return (
         <div className="block">
             <div className="block-header">
                 <Circle color={color} />
                 <h2 className="block-title">{title}</h2>
-                <span className={`block-status color-${color}`}>{status}</span>
+
+                <div className="block-align-div">
+                <span className="block-align-span">{UpdateDate} </span>   
+                <span className={`block-status color-${color}`}>{status}</span>  
+               </div>
             </div>
             <div className="block-content" dangerouslySetInnerHTML={{ __html: content }}>
+            
             </div>
         </div>
     );
