e47547f9546aaa730dc05679f7d4c14f98376193
Joel <joel.hamilton@fusionnetworks.co.nz>
2018-05-24 15:21:51 +1200

    update css for google Sign-In Branding Guidelines

--- src/FirstLogin.js
+++ src/FirstLogin.js
@@ -1,16 +1,16 @@
 import React from 'react';
 
 export default function({onSignInClick, isInitialLoading}) {
-    
+    // first-login-button button-main needs to be reviewed
     return (
         <div className="first-login">
             <img src={`${process.env.PUBLIC_URL}/logo-vertical.png`} alt="Manaiakalani" />
             <button
-                className="first-login-button button-main"
+				className={isInitialLoading ? "google-login-button google-login-button-diableled" : "google-login-button google-login-button-reddy"}
                 type="button"
                 onClick={onSignInClick}
             >
-                {isInitialLoading ? 'Loading....' : 'Login with Google'}
+                'Loading....' 
             
             </button>
         
